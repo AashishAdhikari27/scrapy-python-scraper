07a2f02fee8204c9587d6d84a28bba7f17c8d489
AashishAdhikari27 <aashishadhikari2027@gmail.com>
2025-03-29 23:02:52 +0545

    extracted name,price,url

--- 02)first_scrapy_spider/bookscraper/bookscraper/spiders/bookspider.py
+++ 02)first_scrapy_spider/bookscraper/bookscraper/spiders/bookspider.py
@@ -6,5 +6,28 @@ class BookspiderSpider(scrapy.Spider):
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com"]
 
+
+# parse function acts as the brain on which the spider operates
+
     def parse(self, response):
-        pass
+        books = response.css('article.product_pod')
+
+        for book in books:
+
+            yield{ # this just extract the name ,price and url from the first page
+                'name' : book.css('h3 a::text').get() ,
+                'price' : book.css('.product_price .price_color::text').get() ,
+                'image_url' : book.css('h3 a').attrib['href'],
+            }
+        
+        next_page = response.css('li.next a').attrib['href']  # next page url dinxa
+
+        if next_page is not None:
+
+            if 'catalogue/' in next_page:
+                next_page_url = 'https://books.toscrape.com/' + next_page 
+            else:
+                next_page_url = 'https://books.toscrape.com/catalogue/' + next_page 
+
+            yield response.follow(next_page_url, callback= self.parse) # this will call the parse function again on the next page
+ 
